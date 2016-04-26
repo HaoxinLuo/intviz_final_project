6cc8b3da6aefe82d372a182a0341c6173415480a
ren <luoh2@rpi.edu>
2016-04-26 10:46:00 -0400

    made slices render in correct order(costly)

--- piecharts.js
+++ piecharts.js
@@ -11,7 +11,7 @@ function loadJSON(callback) {
     //http://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
     var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'data/data.json', true); // Replace 'my_data' with the path to your file
+    xobj.open('GET', 'data.json', true); // Replace 'my_data' with the path to your file
     xobj.onreadystatechange = function () {
           if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
@@ -23,23 +23,23 @@ function loadJSON(callback) {
 
 function drawPieCharts(response){
     var data = JSON.parse(response);
-    points = [];
+//    points = [];
     for(var borough in data){
 	for(var zipcode in data[borough]){
 	    for(var loc in data[borough][zipcode]){
 		place = data[borough][zipcode][loc]
 		//createPieChart(place).addTo(mymap);
 		var pie = createPieChart(place);
-		points.push(pie);
+//		points.push(pie);
 	    }
 	}
     }
-    points.sort(function(a,b){
-	return b._numPpl - a._numPpl;
-    });
-    for(var point in points){
-	points[point].addTo(mymap);
-    }
+    // points.sort(function(a,b){
+    // 	return b._numPpl - a._numPpl;
+    // });
+    // for(var point in points){
+    // 	//points[point].addTo(mymap);
+    // }
 }
 
 function createPieChart(place){
@@ -50,7 +50,7 @@ function createPieChart(place){
 	if (!(race in data)){data[race]=0;}
 	data[race] +=1;
     }
-    var group = L.layerGroup();
+    // var group = L.layerGroup();	
     var startAngle = 0;
     for(var race in data){
 	var color = getColor(race);
@@ -59,25 +59,73 @@ function createPieChart(place){
 	var endAngle = (data[race]/total)*360.0+startAngle;
 	var slice = L.circle([x,y],total*15,{
 	    color:color, startAngle:startAngle,stopAngle:endAngle})
-	group.addLayer(slice);
+	// group.addLayer(slice);
+	addToOverlay(race,slice);
 	slice.bindPopup(getRace(race)+":"+data[race]);
 	startAngle = endAngle;
     }
-    group._numPpl = total;
-    return group;
+    // group._numPpl = total;
+    // return group;
+}
+
+function addToOverlay(race,slice){
+    overlays['All'].addLayer(slice);
+    overlays[getRace(race)].addLayer(slice);
 }
 
 function getRace(race){
     races = {'A':'Asian/Pacific Islander','B':'Black',
 	     'I':'American Indian/Alaskan Native','P':'Black-Hispanic',
-	     'Q':'White-Hispanic','W':'White','X':'Unknown','Z':'Other'};
+	     'Q':'White-Hispanic','W':'White','U':'Unknown','Z':'Other'};
     return races[race];
 }    
 
 function getColor(race){
     races = {'A':'#1b9e77','B':'#d95f02','I':'#7570b3','P':'#e7298a',
-	     'Q':'#66a61e','W':'#e6ab00','X':'#a6761d','Z':'#666666'};
+	     'Q':'#66a61e','W':'#e6ab00','U':'#a6761d','Z':'#666666'};
     return races[race];
 }
 
+function bringToFront(e){
+    activeSlices = []
+    for(var race in overlays){
+	overlay = overlays[race];
+	if(overlayActive(overlay)){
+	    layers = overlay.getLayers();
+	    for(var layer in layers){
+		var slice = layers[layer];
+		activeSlices.push(slice);
+	    }
+	}
+    }
+    activeSlices.sort(function(a,b){return b.options.radius-a.options.radius;});
+    for(var slice in activeSlices){
+	activeSlices[slice].bringToFront();
+    }
+}
+
+function overlayActive(overlay){
+    try{
+	return overlay.getBounds() != null;
+    }catch(e){
+	return false;
+    }
+}
+
+var overlays = {
+    "All": new L.featureGroup(),
+    "Asian/Pacific Islander": new L.featureGroup(),
+    "Black": new L.featureGroup(),
+    "American Indian/Alaskan Native": new L.featureGroup(),
+    "Black-Hispanic": new L.featureGroup(),
+    "White-Hispanic": new L.featureGroup(),
+    "White": new L.featureGroup(),
+    "Unknown": new L.featureGroup(),
+    "Other": new L.featureGroup()
+};
+
 loadJSON(drawPieCharts);
+L.control.layers(null, overlays,{autoZIndex:false}).addTo(mymap);
+mymap.on("overlayadd",bringToFront);
+
+
